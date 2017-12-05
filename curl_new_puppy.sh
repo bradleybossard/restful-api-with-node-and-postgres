ce8bc8a98e1bd0bd3ce0f82a17a3cbec56ad157a
Bradley Bossard <bradleybossard@gmail.com>
2017-12-04 22:51:03 -0600

    Adding curls to run against db

---
+++ curl_new_puppy.sh
@@ -0,0 +1,2 @@
+curl -X POST -d "name=Derby&breed=annoying&age=3&sex=f" \
+http://0.0.0.0:3000/api/puppies
---
+++ curl_update_puppy.sh
@@ -0,0 +1,2 @@
+curl -X PUT --data "name=Hunter&breed=annoying&age=33&sex=m" \
+http://127.0.0.1:3000/api/puppies/1
